3b06a7e53acbd02a4d4064be1a56feb280f495ed
Idan Izhaki <izhaki.idan@gmail.com>
2019-05-29 15:30:56 +0300

    Updated manifest

--- public/sw.js
+++ public/sw.js
@@ -1,74 +1,35 @@
-var CURRENT_CACHES = 'v1'
-
+const PRECACHE_URLS = [
+    'index.html',
+]
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js', {scope: './'})
+    navigator.serviceWorker.register('sw.js', {scope: './'})
     .then(registration => {
-        caches.open('v1', (cache) => {
-            cache.put(event.req)
-        })
-        self.addEventListener('fetch', event => {
-            if(event.request.method != 'GET') return
-            
-            event.respondWith(async () => {
-                const cache = await caches.open('v1')
-                const cachedResponse = await cache.match(event.request)
-                
-                if(cachedResponse) {
-                    event.waitUntil(cache.add(event.request))
-                    return cachedResponse
-                }
-                return fetch(event.request)
-            })
-        })
-        console.log("SW Registered")
+        console.log(registration)
     })
-    .catch(err => console.error("Failed to registed SW: ", err))
-} else {
-    console.log("Serviceworker not supported")
-}
+    .catch(err => console.error(err))
+} 
+self.addEventListener('install', event => {
+    console.log("Installing SW....")
+    event.waitUntil(
+        caches.open('v1')
+        .then(cache => {
+            cache.addAll(PRECACHE_URLS)
+        })
+        .then(self.skipWaiting())
+    )
+})
 
-// self.addEventListener('activate', function(event) {
-//     var expectedCacheNames = Object.values(CURRENT_CACHES);
-//     event.waitUntil(
-//         caches.keys().then(function(cacheNames) {
-//             return Promise.all(
-//                 cacheNames.map(function(cacheName) {
-//                     if (!expectedCacheNames.includes(cacheName)) {
-//                         console.log('Deleting out of date cache:', cacheName);
-                        
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-        
-        
-// if((event.request.url.indexOf('http') === 0)){
-//     self.addEventListener('fetch', (event) => {
-//         event.respondWith(
-//             caches.open('v1')
-//             .then(cache => {
-//                 return cache.match(event.request)
-//                 .then(response => {
-//                     if(response) {
-//                         console.log("Responded with cache")
-//                         return response
-//                     }
-//                     return fetch(event.request).then((networkResponse) => {
-//                         cache.addAll([event.request])
-//                         console.group()
-//                         console.log("Cache stored")
-//                         console.log("Responded with network response")
-//                         console.groupEnd()
-//                         return networkResponse
-//                     })
-//                 }).catch(err => console.error("Unable to fetch: ", err))
-//             })
-//             )
-//         })
-// }
-        
-            
-            
+self.addEventListener('fetch', function fetcher (event) {
+    var request = event.request;
+    // check if request 
+    if (request.url.indexOf('assets.contentful.com') > -1) {
+      // contentful asset detected
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+            // return from cache, otherwise fetch from network
+                return response || fetch(request);
+            })
+        );
+    }
+    // otherwise: ignore event
+});
